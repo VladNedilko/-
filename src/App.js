98fcd42611c9a4d767a8d9b618eec9daec3b7c75
Vlad <nedilko4444466@gmail.com>
2024-10-01 21:20:20 +0300

    неземная любовь с курткой

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [status, toggleStatus] = useStatus('inactive');
+  useLogData(status);
+
   return (
     <div className="App">
       <header className="App-header">
